6292b302b59f6e57f91e845921b534480d9dd274
Beatty, Annette <Annette_Beatty@intuit.com>
2018-10-19 18:41:44 -0700

    Cleaned up textContoller

--- controllers/textController.js
+++ controllers/textController.js
@@ -1,13 +1,8 @@
-//var twilioSid = process.env.TWILIO_SID;
-//var twilioToken = process.env.TWILIO_TOKEN;
-const twilioSid = "AC5b6619d067d9e174372a8044c602cc43";
-const twilioToken = "0b8392a77a1a8425327ac343ee3a4f26";
-
 // Read and set environment variables
 require("dotenv").config();
 
-console.log("SID ", process.env.TWILIO_SID);
-console.log("TOKEN ", process.env.TWILIO_TOKEN);
+const twilioSid = process.env.TWILIO_SID;
+const twilioToken = process.env.TWILIO_TOKEN;
 
 const client = require("twilio")(twilioSid, twilioToken);
 
@@ -16,7 +11,7 @@ module.exports = {
   create: function(req, res) {
     console.log("text = ", req.body.text);
     console.log("cell = ", req.body.cell);
-    var msg = "FitBet: Update to " + req.body.text + "  challenge - URL";
+    var msg = "FitBet: " + req.body.text + "  challenge - <URL link here>";
     console.log("Texting this ", req.body.text);
     // Send a text
     client.messages
